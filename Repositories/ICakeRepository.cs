b8ff75cf26706ce1d749c29a87257098b13f0c02
ftiitk <106618659+ftiitk@users.noreply.github.com>
2022-07-11 08:57:11 +0430

    Add files via upload

---
+++ Repositories/ICakeRepository.cs
@@ -0,0 +1,17 @@
+﻿using SweetShoppp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SweetShoppp.Repositories
+{
+    public interface ICakeRepository
+    {
+        Task<IEnumerable<Cake>> Get();
+        Task<Cake> Get(int id);
+        Task<Cake> Create(Cake Cake);
+        Task Update(Cake Cake);
+        Task Delete(int id);
+    }
+}
