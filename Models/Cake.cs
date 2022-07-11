8f5a0573cb2d74c89b5da7cee869b10dd5f640a5
ftiitk <106618659+ftiitk@users.noreply.github.com>
2022-07-11 08:48:09 +0430

    Add files via upload

---
+++ Models/Cake.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SweetShoppp.Models
+{
+    public class Cake
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public decimal Price { get; set; }
+        public string? Category { get; set; }
+        public string? Caption { get; set; }
+        
+    }
+
+}
---
+++ Models/CakeContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SweetShoppp.Models
+{
+    public class CakeContext: DbContext
+    {
+        public CakeContext(DbContextOptions<CakeContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Cake> Cakes { get; set; }
+    }
+}
